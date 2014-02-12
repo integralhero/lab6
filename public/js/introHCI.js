1c6dc9cbf9fe64dff827df6c272af37be15b4d00
David Jiang <dnjiang@stanford.edu>
2014-02-12 15:21:59 -0800

    Update

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,63 +10,40 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$('.project a').click(addProjectDetails);
-
-	$('#colorBtn').click(randomizeColors);
+	$('#colorBtn').click(getColor);
 }
 
-/*
- * Make an AJAX call to retrieve project details and add it in
- */
+function clickProject(result) {
+	//console.log(result);
+	var id = result["id"];
+	var insert = 
+		'<h3>' + result.title + '</h3> ' + 
+		'<img src="' + result.image + '" class="detailsImage">' +
+		'<p><small>' + result.date + '</small></p>' +
+		'<p>' + result.summary + '</p>';
+	$("div#project" + id + " div.details").html(insert);
+}
+
 function addProjectDetails(e) {
 	// Prevent following the link
 	e.preventDefault();
+	
+	var id = $(this).closest('.project').attr('id');
+	var idNumber = id.substr('project'.length);
 
-	// Get the div ID, e.g., "project3"
-	var projectID = $(this).closest('.project').attr('id');
-	// get rid of 'project' from the front of the id 'project3'
-	var idNumber = projectID.substr('project'.length);
-
-	console.log("User clicked on project " + idNumber);
-
-	var url = "/project/" + idNumber;
-	$.get(url, addProject);
+	$.get("/project/" + idNumber, clickProject);
 }
 
-function addProject(result) {
-	console.log(result);
-	var idNumber = result.id;
-	var detailsSelector = "div#project" + idNumber + " div.details";
-
-	var htmlInsert = 
-		'<img src="' + result.image + '" class="detailsImage">' +
-		'<h3>' + result.title + '</h3>' +
-		'<p><small>' + result.date + '</small></p>' +
-		'<p>' + result.summary + '</p>';
-
-	//result.id. result.title, result.date, result.summary, result.image
-	//include image, small header with date, and summary
-	//Give the image the class detailsImage
-
-	$(detailsSelector).html(htmlInsert);
-}
-
-/*
- * Make an AJAX call to retrieve a color palette for the site
- * and apply it
- */
-function randomizeColors(e) {
-	console.log("User clicked on color button");
-	$.get("/palette", applyPalette);
+function getColor(e) {
+	$.get("/palette", function(result) {
+		var colors = result.colors.hex;
+		console.log(colors.length);
+		$('body').css('background-color', colors[0]);
+		$('.thumbnail').css('background-color', colors[1]);
+		$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+		$('p').css('color', colors[3]);
+		$('.project img').css('opacity', .75);
+	});
 
 }
 
-function applyPalette(result) {
-	console.log(result);
-	var colors = result.colors.hex;
-
-	$('body').css('background-color', colors[0]);
-	$('.thumbnail').css('background-color', colors[1]);
-	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
-	$('p').css('color', colors[3]);
-	$('.project img').css('opacity', .75);
-}
--- routes/palette.js
+++ routes/palette.js
@@ -1,8 +1,6 @@
 var palettes = require('../palettes.json');
 
 exports.randomPalette = function(req, res) {
-	// get a random palette from the top ones
-	var randomPalette = palettes[Math.floor(palettes.length * Math.random())];
-	res.json(randomPalette);
-	// res.send('Your random palette is called: ' + randomPalette['title']);
+	var random = palettes[Math.floor(palettes.length * Math.random())];
+	res.json(random);
 }
